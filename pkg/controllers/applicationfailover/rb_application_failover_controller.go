d2a98992338699406922b52d3653db9af51bc844
Aditya Addepalli <dyex719@gmail.com>
2024-07-18 16:04:54 -0400

    Add application failover flag
    Signed-off-by: Aditya Addepalli <dyex719@gmail.com>

--- pkg/controllers/applicationfailover/rb_application_failover_controller.go
+++ pkg/controllers/applicationfailover/rb_application_failover_controller.go
@@ -23,10 +23,12 @@ import (
 	"time"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/retry"
 	"k8s.io/klog/v2"
 	"k8s.io/utils/ptr"
 	controllerruntime "sigs.k8s.io/controller-runtime"
@@ -152,8 +154,45 @@ func (c *RBApplicationFailoverController) syncBinding(binding *workv1alpha2.Reso
 	return time.Duration(duration) * time.Second, nil
 }
 
+func (c *RBApplicationFailoverController) updateFailoverStatus(binding *workv1alpha2.ResourceBinding, cluster string) (err error) {
+	message := fmt.Sprintf("Failover triggered for replica on cluster %s", cluster)
+	newFailoverAppliedCondition := metav1.Condition{
+		Type:               workv1alpha2.EvictionReasonApplicationFailure,
+		Status:             metav1.ConditionTrue,
+		Reason:             "ApplicationFailoverSuccessful",
+		Message:            message,
+		LastTransitionTime: metav1.Now(),
+	}
+
+	err = retry.RetryOnConflict(retry.DefaultRetry, func() (err error) {
+		_, err = helper.UpdateStatus(context.Background(), c.Client, binding, func() error {
+			// set binding status with the newest condition
+			currentTime := metav1.Now()
+			failoverHistoryItem := workv1alpha2.FailoverHistoryItem{
+				FailoverTime:  &currentTime,
+				OriginCluster: cluster,
+			}
+			binding.Status.FailoverHistory = append(binding.Status.FailoverHistory, failoverHistoryItem)
+			klog.V(4).Infof("Failover history is %+v", binding.Status.FailoverHistory)
+			meta.SetStatusCondition(&binding.Status.Conditions, newFailoverAppliedCondition)
+			return nil
+		})
+		return err
+	})
+
+	if err != nil {
+		klog.Errorf("Failed to update condition of binding %s/%s: %s", binding.Namespace, binding.Name, err.Error())
+	}
+	return nil
+}
+
 func (c *RBApplicationFailoverController) evictBinding(binding *workv1alpha2.ResourceBinding, clusters []string) error {
 	for _, cluster := range clusters {
+		klog.V(4).Info("Updating resource binding with latest failover timestamp for cluster %s.", cluster)
+		updateerr := c.updateFailoverStatus(binding, cluster)
+		if updateerr != nil {
+			klog.Errorf("Failed to update status with failover information.")
+		}
 		switch binding.Spec.Failover.Application.PurgeMode {
 		case policyv1alpha1.Graciously:
 			if features.FeatureGate.Enabled(features.GracefulEviction) {
